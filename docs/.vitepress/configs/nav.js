95b3621f0d005eee94e68eeab2e33a72ab196cc8
reoreo <768119359@qq.com>
2021-09-27 21:23:09 +0800

    更新 nav.js

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -5,5 +5,5 @@ module.exports = [
   { text: 'css', link: '/css/盒子模型', activeMatch: getPath('^/css/') },
   { text: '框架', link: '/框架/vue', activeMatch: getPath('^/框架/') },
   { text: '数据库', link: '/数据库/mongodb' },
-  { text: '笔试、面试记录', link: '/笔试、面试记录/' },
+  { text: '笔试、面试记录', link: '/笔试、面试记录/我的笔试、面试记录.md' },
 ]
